c484787630ec37202eb044803725d7537c60bf4a
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-09-29 19:45:39 +0300

    lessons: 9

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {noop} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
 
 @Component({
   selector: 'about',
@@ -15,62 +12,5 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    // document.addEventListener('click', () => {
-    //   console.log('click');
-    //
-    //   setTimeout(() => {
-    //     console.log('timeout');
-    //
-    //
-    //     let counter = 0;
-    //
-    //     setInterval(() => {
-    //       console.log(counter);
-    //       counter++;
-    //     }, 1000);
-    //
-    //   }, 3000);
-    //
-    // });
-
-
-    // const interval$ = interval(1000);
-    //
-    // interval$.subscribe(v => console.log(`stream 1 => ${v}`));
-    // interval$.subscribe(v => console.log(`stream 2 => ${v}`));
-
-    // const interval$ = timer(3000, 1000);
-    //
-    // const sub = interval$.subscribe(
-    //   v => console.log(`stream 1 => ${v}`),
-    //   err => console.log(err),
-    //   () => console.log('completed')
-    // );
-    //
-    // setTimeout(() => sub.unsubscribe(), 5000);
-    //
-    //
-    // const click$ = fromEvent(document, 'click');
-    //
-    // click$.subscribe(
-    //   evt => console.log(evt),
-    //   err => console.log(err),
-    //   () => console.log('complete')
-    // );
-
-
-    const http$ = createHttpObservable('api/courses');
-
-
-    const courses$ = http$.pipe(
-      map(res => res.payload)
-    );
-
-    courses$
-      .subscribe(
-        response => console.log(response),
-        noop,
-        () => console.log('completed')
-      );
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,40 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {Course} from '../model/course';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses: Course[];
+  advanceCourses: Course[];
+
+  ngOnInit() {
+    const http$ = createHttpObservable('api/courses');
 
 
-    constructor() {
+    const courses$ = http$.pipe(
+      map(res => res.payload)
+    );
 
-    }
+    const byType = type => (item, i, ar) => item.category === type;
 
-    ngOnInit() {
+    courses$
+      .subscribe(
+        response => {
+          this.beginnerCourses = response.filter(byType('BEGINNER'));
+          this.advanceCourses = response.filter(byType('ADVANCED'));
+        },
+        noop,
+        () => console.log('completed')
+      );
 
 
-
-    }
+  }
 
 }
