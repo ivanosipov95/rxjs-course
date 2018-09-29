e3092554d983ed675ffcb1963d4efc66fe3034d8
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-09-29 21:46:46 +0300

    lessons: 12

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {concat, of} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -12,5 +13,16 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
+    const stream$1 = of(1, 2, 3);
+    const stream$2 = of(4, 5, 6);
+    const stream$3 = of(7, 8, 9);
+
+
+    concat(stream$1, stream$2, stream$3)
+      .subscribe(
+        console.log,
+        console.log,
+        () => console.log('complete')
+      );
   }
 }
