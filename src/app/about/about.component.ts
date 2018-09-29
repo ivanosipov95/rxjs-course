759ae4bd95d1fd772cb7e43672361c276ce66fc9
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-09-29 19:13:45 +0300

    lessons: [1-8]

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {noop} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 @Component({
   selector: 'about',
@@ -7,9 +10,67 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+    // document.addEventListener('click', () => {
+    //   console.log('click');
+    //
+    //   setTimeout(() => {
+    //     console.log('timeout');
+    //
+    //
+    //     let counter = 0;
+    //
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //
+    //   }, 3000);
+    //
+    // });
+
+
+    // const interval$ = interval(1000);
+    //
+    // interval$.subscribe(v => console.log(`stream 1 => ${v}`));
+    // interval$.subscribe(v => console.log(`stream 2 => ${v}`));
+
+    // const interval$ = timer(3000, 1000);
+    //
+    // const sub = interval$.subscribe(
+    //   v => console.log(`stream 1 => ${v}`),
+    //   err => console.log(err),
+    //   () => console.log('completed')
+    // );
+    //
+    // setTimeout(() => sub.unsubscribe(), 5000);
+    //
+    //
+    // const click$ = fromEvent(document, 'click');
+    //
+    // click$.subscribe(
+    //   evt => console.log(evt),
+    //   err => console.log(err),
+    //   () => console.log('complete')
+    // );
+
+
+    const http$ = createHttpObservable('api/courses');
+
+
+    const courses$ = http$.pipe(
+      map(res => res.payload)
+    );
+
+    courses$
+      .subscribe(
+        response => console.log(response),
+        noop,
+        () => console.log('completed')
+      );
+  }
 }
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,14 @@
+import {Observable} from 'rxjs';
 
+export function createHttpObservable(url: string): Observable<any> {
+  return Observable.create(observer => {
+    fetch(url)
+      .then(res => res.json())
+      .then(response => {
+        observer.next(response);
+        observer.complete();
+      })
+      .catch(err => observer.error(err));
+  });
+}
 
