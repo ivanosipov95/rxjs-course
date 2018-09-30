8660199c584cb5d7861b12b14004966e44ed6412
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-09-30 15:25:50 +0300

    lessons: 14

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,59 +1,78 @@
 import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {Course} from '../model/course';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
+import {fromEvent, interval, Observable, of} from 'rxjs';
+import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, switchMap} from 'rxjs/operators';
 import {fromPromise} from 'rxjs/internal-compatibility';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
 })
 export class CourseDialogComponent implements OnInit, AfterViewInit {
 
-    form: FormGroup;
-    course:Course;
+  form: FormGroup;
+  course: Course;
 
-    @ViewChild('saveButton') saveButton: ElementRef;
+  @ViewChild('saveButton') saveButton: ElementRef;
 
-    @ViewChild('searchInput') searchInput : ElementRef;
+  @ViewChild('searchInput') searchInput: ElementRef;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course) {
 
-        this.course = course;
+    this.course = course;
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
 
-    }
+  }
 
-    ngOnInit() {
-
-
-
-    }
-
-
-
-    ngAfterViewInit() {
+  ngOnInit() {
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid),
+      concatMap(changes => this.saveCourse(changes))
+    )
+      .subscribe(console.log);
+  }
 
 
-    }
+  saveCourse(changes) {
+    // return Observable.create(observer => {
+    //   setTimeout(() => {
+    //     observer.next('done');
+    //     observer.complete();
+    //   }, 3000);
+    // });
+    return fromPromise(
+      fetch(`/api/courses/${this.course.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(changes),
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+    );
+  }
 
 
+  ngAfterViewInit() {
 
-    close() {
-        this.dialogRef.close();
-    }
+
+  }
+
+
+  close() {
+    this.dialogRef.close();
+  }
 
 }
