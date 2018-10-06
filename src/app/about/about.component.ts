afe2ee9a4b8fba1de187054d43b08a7bc3215a21
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-10-06 21:33:11 +0300

    lessons: 31

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,20 +1,27 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+  ngOnInit() {
+    const subject = new Subject();
 
+    const observable$ = subject.asObservable();
 
-    }
+    observable$.subscribe(console.log);
+
+    subject.next(1);
+    subject.next(1);
+    subject.next(1);
+  }
 
 
 }
