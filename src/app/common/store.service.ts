9cba04d07d1f5354338e6757ee1dcf4253d1d71b
Ivan Osipov <ivan.osipov.95@gmail.com>
2018-10-07 00:32:38 +0300

    lessons: 36

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -3,6 +3,7 @@ import {Course} from '../model/course';
 import {delayWhen, map, retryWhen, shareReplay, subscribeOn, tap} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {createHttpObservable} from './util';
+import {fromPromise} from 'rxjs/internal-compatibility';
 
 @Injectable({
   providedIn: 'root'
@@ -23,18 +24,42 @@ export class Store {
   }
 
   selectBeginnerCourses() {
-    return this.courses$
-      .pipe(
-        map(courses => courses
-          .filter(course => course.category === 'BEGINNER'))
-      );
+    return this.fetchCoursesByCategory('BEGINNER');
   }
 
   selectAdvancedCourses() {
+    return this.fetchCoursesByCategory('ADVANCED');
+  }
+
+
+  saveCourse(courseId, changes): Observable<any> {
+    const courses = this.subject.getValue();
+
+    const courseIndex = courses.findIndex(course => course.id === courseId);
+
+    const newCourses = courses.slice(0);
+
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    };
+
+    this.subject.next(newCourses);
+
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }));
+  }
+
+  private fetchCoursesByCategory(category: string): Observable<Course[]> {
     return this.courses$
       .pipe(
         map(courses => courses
-          .filter(course => course.category === 'ADVANCED'))
+          .filter(course => course.category === category))
       );
   }
 }
